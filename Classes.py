d7b8ebc2d5ce83b31d34d501ac67bc1854eae408
Assil <assilmahfoudi@gmail.com>
2022-11-13 17:40:50 +0100

    GoalCard Class added

--- Classes.py
+++ Classes.py
@@ -7,26 +7,20 @@ class Card():
         self.name=name
         self.function=function
     
+    def display_card(self):
 
-    def reveal(self):
-
+        fst=" "*len(self.name)
         snd=self.name
-        shwd= f"""
-        ({" "*len(self.name)})
-        ({snd})
-        ({" "*len(self.name)})
-        """
-        print(shwd)
-
+        thd=" "*len(self.name)
+        shwd= f"({fst})\n({snd})\n({thd})"
+        return shwd
 
     def __str__(self):
         return f"{self.name} ({self.function}) card "
 
 
 class PathCard(Card):
-
-    def reveal(self):
-
+    def display_card(self):
         fst="   "
         snd=f" {self.function[1]} "
         thd="   "
@@ -41,12 +35,11 @@ class PathCard(Card):
             thd=" | "
     
         shwd= f"({fst})\n({snd})\n({thd})"
-        print(shwd)
+        return shwd
 
 
 class ActionCard(Card):
-    def reveal(self):
-
+    def display_card(self):
         fst=f" {self.name[0]} "
         snd=f"{self.name}"
         thd=f" {self.name[2]} "
@@ -59,7 +52,7 @@ class ActionCard(Card):
             snd=f" {self.name[0]} "
             thd=f"   "
         shwd= f"({fst})\n({snd})\n({thd})"
-        print(shwd)
+        return shwd
 
 
 class GoldCard(Card):
@@ -67,24 +60,64 @@ class GoldCard(Card):
     def __init__(self, name, function="Nu",gain=1):
         Card.__init__(self,name,function)
         self.gain=gain
-    def reveal(self):
 
+    def display_card(self):
         fst=f" {self.gain} "
         snd=f"{self.name}"
         thd=f"   "
         shwd= f"({fst})\n({snd})\n({thd})"
-        print(shwd)
+        return shwd
+
+
+class GoalCard(PathCard):
+    def __init__(self, name, function="Nu",value='N'):
+        PathCard.__init__(self,name,function)
+        self.value=value
+
+    def showcard(self):
+        fst="   "
+        snd=f" {self.value} "
+        thd="   "
+
+        if "U" in self.name:
+            fst=" | "
+        if "L" in self.name:
+            snd="-"+snd[1:]
+        if "R" in self.name:
+            snd=snd[:1]+"-"
+        if "D" in self.name:
+            thd=" | "
+    
+        shwd= f"({fst})\n({snd})\n({thd})"
+        return shwd
+        
+    def display_card(self):
+        fst=f"   "
+        snd=f"END"
+        thd=f"   "
+        shwd= f"({fst})\n({snd})\n({thd})"
+        return shwd
+
+
+
+
 '''
 carta=PathCard("UDL",'Nx')
 print(carta)
-carta.reveal()
+carta.display_card()
 carta=ActionCard("MAP",'Nu')
 print(carta)
-carta.reveal()
+carta.display_card()
 carta=ActionCard("Lit",'N+')
 print(carta)
-carta.reveal()
+carta.display_card()
 carta=GoldCard("Gld",'Nu',4)
 print(carta)
-carta.reveal()
+carta.display_card()
+
+carta=GoalCard("ULD",'Nu','N')
+print(carta)
+dis=carta.display_card()
+print(dis)
+print(carta.showcard())
 '''
