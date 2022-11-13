60e657a7fe795ccc4c25fc452e53e5e6865a69fb
Assil <assilmahfoudi@gmail.com>
2022-11-13 15:43:09 +0100

    Defining Class Card

--- Classes.py
+++ Classes.py
@@ -1,1 +1,26 @@
 print("Here's where we can code the Classes")
+
+# Definition of the Mother class CARD
+class Card():
+
+    def __init__(self,name,function="Nu"):
+        self.name=name
+        self.function=function
+    
+
+    def reveal(self):
+        a= f"""
+        ({" "*len(self.name)})
+        ({self.name})
+        ({" "*len(self.name)})
+        """
+        print(a)
+
+
+    def __str__(self):
+        return f"{self.name} ({self.function}) card "
+
+
+#carta=Card("SAB")
+#print(carta)
+#carta.reveal()
