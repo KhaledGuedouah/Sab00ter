c4e9b108f46a1a69c00caf1ed1e7317698e99388
Assil <assilmahfoudi@gmail.com>
2022-11-13 18:47:55 +0100

    Adding The dec

--- Classes.py
+++ Classes.py
@@ -40,17 +40,19 @@ class PathCard(Card):
 
 class ActionCard(Card):
     def display_card(self):
-        fst=f" {self.name[0]} "
-        snd=f"{self.name}"
-        thd=f" {self.name[2]} "
+
         if self.function == "Nx":
             fst=f"ATT"
-            snd=f" {self.name[0]} "
+            snd=f"{self.name}"
             thd=f"   "
         elif self.function == "N+":
             fst=f"DIF"
-            snd=f" {self.name[0]} "
+            snd=f"{self.name}"
             thd=f"   "
+        else:
+            fst=f" {self.name[0]} "
+            snd=f"{self.name}"
+            thd=f" {self.name[2]} "
         shwd= f"({fst})\n({snd})\n({thd})"
         return shwd
 
@@ -74,7 +76,7 @@ class GoalCard(PathCard):
         PathCard.__init__(self,name,function)
         self.value=value
 
-    def showcard(self):
+    def show_card(self):
         fst="   "
         snd=f" {self.value} "
         thd="   "
@@ -84,7 +86,7 @@ class GoalCard(PathCard):
         if "L" in self.name:
             snd="-"+snd[1:]
         if "R" in self.name:
-            snd=snd[:1]+"-"
+            snd=snd[:2]+"-"
         if "D" in self.name:
             thd=" | "
     
@@ -99,6 +101,90 @@ class GoalCard(PathCard):
         return shwd
 
 
+
+def create_dec():
+    card_list=[]
+
+# 4 cards Saboteurs
+    for _ in range(4):
+        S=Card("SAB")
+        card_list.append(S)
+
+# 7 cards Saboteurs
+    for _ in range(7):
+        S=Card("CHR")
+        card_list.append(S)
+
+# 6 cards End reveal
+    for _ in range(6):
+        S=ActionCard("MAP")
+        card_list.append(S)
+
+# 3 cards Rock fall
+    for _ in range(3):
+        S=ActionCard("RoF")
+        card_list.append(S)
+
+# 2 cards P repair
+    for _ in range(2):
+        S=ActionCard(" P ","N+")
+        card_list.append(S)
+
+# 2 cards Light repair
+    for _ in range(2):
+        S=ActionCard(" Li","N+")
+        card_list.append(S)
+
+# 2 cards Wagon repair  
+    for _ in range(2):
+        S=ActionCard(" W ","N+")
+        card_list.append(S)
+
+# 3 cards P break
+    for _ in range(3):
+        S=ActionCard(" P ","Nx")
+        card_list.append(S)
+
+# 3 cards Light break
+    for _ in range(3):
+        S=ActionCard(" Li","Nx")
+        card_list.append(S)
+
+# 3 cards Wagon break    
+    for _ in range(3):
+        S=ActionCard(" W ","Nx")
+        card_list.append(S)
+
+ # 3 cards combination of reparations
+    S=ActionCard("LiW","N+")
+    card_list.append(S)
+    S=ActionCard("LiP","N+")
+    card_list.append(S)
+    S=ActionCard(" PW","N+")
+    card_list.append(S)
+
+#3 goal cards
+
+    S=GoalCard("UR",value='N')
+    card_list.append(S)
+    S=GoalCard("UL",value='N')
+    card_list.append(S)
+    S=GoalCard("URLD",value='G')
+    card_list.append(S)
+
+    return card_list
+
+p=create_dec()
+
+for i in p:
+    print(i.display_card())
+    print("\n")
+
+for i in range(1,4):
+    print(p[-i].show_card())
+    print('\n')
+
+    
 
 
 '''
